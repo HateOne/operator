02ba4d5bd14da3a19dcefdd18ac32e79b73930c8
Nikita <khramov-2016@inbox.ru>
2023-11-11 19:42:41 +0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 13676;
+        int percent = 5 ;
+
+        int bonus=amount / 100 * percent;
+
+        System.out.println("Итоговый бонус:" +bonus);
+
+    }
+}
